7fb9b245e1019fa866f9956922eb1a6321d46d5a
gorkemozucar <serkangorkemozucar@eskisehir.edu.tr>
2022-06-24 09:57:58 +0300

    Initial commit

---
+++ EXAPI/EXAPI/Model/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EXAPI.Model
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+        public DbSet<SuperHero> SuperHeroes => Set<SuperHero>();   
+    }
+}
